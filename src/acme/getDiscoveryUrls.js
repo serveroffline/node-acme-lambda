df262e94e7ace2714bd861021dd0cb0be1b71ba4
Angelique Dawnbringer <AngeliqueDawnbringer@users.noreply.github.com>
2018-08-13 13:37:46 +0200

    Update getDiscoveryUrls.js

--- src/acme/getDiscoveryUrls.js
+++ src/acme/getDiscoveryUrls.js
@@ -3,6 +3,7 @@ const agent = require('superagent')
 
 const getDiscoveryUrls = (discoveryUrl) =>
   agent.get(`${config['acme-directory-url']}/directory`)
+  .set('User-Agent', `${config['agent-name']}/${config['agent-version']}`)
   .then((data) => data.body)
 
 module.exports = getDiscoveryUrls

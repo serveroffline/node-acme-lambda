a6544e1b6f175a2208903849dc71bda8f5cac101
Angelique Dawnbringer <AngeliqueDawnbringer@users.noreply.github.com>
2018-08-13 13:35:01 +0200

    Update getNonce.js

--- src/acme/getNonce.js
+++ src/acme/getNonce.js
@@ -3,6 +3,7 @@ const agent = require('superagent')
 
 const getNonce = () =>
   agent.get(`${config['acme-directory-url']}/directory`)
+  .set('User-Agent', `${config['agent-name']}/${config['agent-version']}`)
   .then((data) => data.header['replay-nonce'])
   .catch((e) => {
     console.error(`Error getting nonce`, e)
